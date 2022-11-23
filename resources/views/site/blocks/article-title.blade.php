d4f23992285b90611ddaf0cf17eebca7164bf6b3
Armin Salihovic <armin.salihovic@live.com>
2022-11-23 20:20:48 +0100

    Improve spacing on title and paragraph blocks

--- resources/views/site/blocks/article-title.blade.php
+++ resources/views/site/blocks/article-title.blade.php
@@ -2,7 +2,7 @@
     <aside class="md:col-span-2 col-span-6">
         <h3 class="text-lg md:text-5xl">{!! $block->input('year') !!}</h3>
     </aside>
-    <div class="md:col-span-4 col-span-6">
+    <div class="md:col-span-4 col-span-6 pr-0 xl:pr-32">
         <h1 class="text-3xl md:text-5xl">{{ $block->input('title') }}</h1>
         <h2 class="text-2xl md:text-4xl text-gray-500">{!! $block->input('subtitle') !!}</h2>
     </div>
--- resources/views/site/blocks/paragraph.blade.php
+++ resources/views/site/blocks/paragraph.blade.php
@@ -2,7 +2,7 @@
     <aside class="md:col-span-2 col-span-6">
         <h4 class="font-bold">{!! $block->input('title') !!}</h4>
     </aside>
-    <div class="md:col-span-4 col-span-6">
+    <div class="block-paragraph md:col-span-4 col-span-6 pr-0 xl:pr-32">
         {!! $block->input('paragraph') !!}
     </div>
 </div>
