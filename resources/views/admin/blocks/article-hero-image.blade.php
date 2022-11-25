7e9218013d3e1de3b416a7149b6c13c282913108
Armin Salihovic <armin.salihovic@live.com>
2022-11-25 14:55:12 +0100

    Add option to choose the position of the hero image

--- resources/views/admin/blocks/article-hero-image.blade.php
+++ resources/views/admin/blocks/article-hero-image.blade.php
@@ -1,6 +1,27 @@
 @twillBlockTitle('Hero image')
 @twillBlockIcon('text')
 
+@formField('select', [
+    'name' => 'position',
+    'label' => 'Position',
+    'placeholder' => 'Select the position',
+    'default' => 2,
+    'options' => [
+        [
+            'value' => 1,
+            'label' => 'Top'
+        ],
+        [
+            'value' => 2,
+            'label' => 'Center'
+        ],
+        [
+            'value' => 3,
+            'label' => 'Bottom'
+        ]
+    ]
+])
+
 @formField('medias', [
     'name' => 'image',
     'label' => 'Image',
--- resources/views/site/blocks/article-hero-image.blade.php
+++ resources/views/site/blocks/article-hero-image.blade.php
@@ -1,8 +1,27 @@
+@php
+    $objectPosition = '';
+    $backgroundPosition = '';
+    switch ($block->input('position')) {
+        case 1:
+            $objectPosition = 'object-position-top';
+            $backgroundPosition = 'bg-position-top';
+            break;
+        case 2:
+            $objectPosition = 'object-position-center';
+            $backgroundPosition = 'bg-position-center';
+            break;
+        case 3:
+            $objectPosition = 'object-position-bottom';
+            $backgroundPosition = 'bg-position-bottom';
+            break;
+    }
+@endphp
+
 <div class="pb-10">
-    <figure class="hidden xs:block img-container mb-3" style="background-image:url('{{ $block->lowQualityImagePlaceholder('image') }}');">
-        <img class="lazyload blur-up w-full w-full bg-center absolute" src="{{ $block->image('image') }}" data-src="{{ $block->image('image') }}" alt="{{ $block->imageAltText('image') }}">
+    <figure class="hidden xs:block img-container mb-3 {{ $backgroundPosition }}" style="background-image:url('{{ $block->lowQualityImagePlaceholder('image') }}');">
+        <img class="lazyload blur-up w-full w-full bg-center absolute {{ $objectPosition }}" src="{{ $block->image('image') }}" data-src="{{ $block->image('image') }}" alt="{{ $block->imageAltText('image') }}">
     </figure>
     <figure class="block xs:hidden img-container image-square mb-3" style="background-image:url('{{ $block->lowQualityImagePlaceholder('image', 'mobile') }}');">
-        <img class="lazyload blur-up w-full w-full bg-center absolute" src="{{ $block->image('image', 'mobile') }}" data-src="{{ $block->image('image', 'mobile') }}" alt="{{ $block->imageAltText('image') }}">
+        <img class="lazyload blur-up w-full w-full bg-center absolute bg-position-top" src="{{ $block->image('image', 'mobile') }}" data-src="{{ $block->image('image', 'mobile') }}" alt="{{ $block->imageAltText('image') }}">
     </figure>
 </div>
