76b6f8e3562820d3a4b84255c4cc2f4f104a7af5
Armin Salihovic <armin.salihovic@live.com>
2022-11-25 12:55:13 +0100

    Queue a job RefreshLQIP on event cms-module.saved

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Jobs\RefreshLQIP;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,7 +28,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen('cms-module.saved', function () {
+            RefreshLQIP::dispatch();
+        });
     }
 
     /**
