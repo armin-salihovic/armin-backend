31bf6b64b0f221d191550ad7662a39cf922d148f
Armin Salihovic <armin.salihovic@live.com>
2022-11-25 13:53:14 +0100

    Add optional lqip.php config file

---
+++ config/lqip.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+//    Modify these values to change the width of the LQIP
+//    According to \A17\Twill\Commands\RefreshLQIP, the default value is 30
+//    To generate LQIP for all images use the command: php artisan twill:lqip --all=1
+
+//    'App\Models\Project' => [
+//        'image' => [
+//            'default' => 100,
+//            'mobile' => 100,
+//            'flexible' => 100,
+//        ],
+//    ],
+//    'App\Models\Page' => [
+//        'image' => [
+//            'default' => 100,
+//            'mobile' => 100,
+//            'flexible' => 100,
+//        ],
+//    ],
+//    'blocks' => [
+//        'image' => [
+//            'default' => 100,
+//            'mobile' => 100,
+//            'flexible' => 100,
+//        ],
+//    ],
+];
