e7b93db2f47effa115dda68f721d58f0724b2348
Armin Salihovic <armin.salihovic@live.com>
2022-12-03 16:01:05 +0100

    Fix margins on article-image

--- resources/views/site/blocks/article-image.blade.php
+++ resources/views/site/blocks/article-image.blade.php
@@ -1,6 +1,6 @@
 <x-block-layout>
-    <div class="pb-8 md:pb-16 {{ $block->input('frame') ? 'block-image-frame' : '' }}">
-        <img class="lazyload w-full" src="{{ $block->lowQualityImagePlaceholder('image') }}" data-src="{{ $block->image('image') }}" alt="{{ $block->imageAltText('image') }}">
+    <div class="mb-8 md:mb-16 {{ $block->input('frame') ? 'block-image-frame' : '' }}">
+        <img class="lazyload w-full" style="height:100%" src="{{ $block->lowQualityImagePlaceholder('image') }}" data-src="{{ $block->image('image') }}" alt="{{ $block->imageAltText('image') }}">
     </div>
 </x-block-layout>
 
