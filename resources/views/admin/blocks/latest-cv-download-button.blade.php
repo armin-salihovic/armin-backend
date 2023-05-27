14016e79097a8dfa0f0b75ba723e09107ec2702d
Armin Salihović <armin.salihovic@live.com>
2023-05-27 13:50:39 +0200

    Add the latest CV download button block

---
+++ resources/views/admin/blocks/latest-cv-download-button.blade.php
@@ -0,0 +1,8 @@
+@twillBlockTitle('Latest CV Download Button')
+@twillBlockIcon('text')
+
+@formField('input', [
+    'name' => 'title',
+    'label' => 'Title',
+    'maxlength' => 100
+])
--- resources/views/admin/pages/form.blade.php
+++ resources/views/admin/pages/form.blade.php
@@ -2,6 +2,6 @@
 
 @section('contentFields')
     @formField('block_editor', [
-        'blocks' => ['paragraph', 'article-paragraph-h3', 'file-download-button', 'double-paragraph', 'article-title', 'article-image', 'article-introduction', 'article-hero-image']
+        'blocks' => ['paragraph', 'article-paragraph-h3', 'file-download-button', 'double-paragraph', 'article-title', 'article-image', 'article-introduction', 'article-hero-image', 'latest-cv-download-button']
     ])
 @stop
