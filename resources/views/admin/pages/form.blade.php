80078292a813754be6a77f4f3bb7db332cd2f754
Armin Salihović <armin.salihovic@live.com>
2023-11-29 18:59:09 +0100

    Add article hero image w/o lazy

--- resources/views/admin/pages/form.blade.php
+++ resources/views/admin/pages/form.blade.php
@@ -2,6 +2,17 @@
 
 @section('contentFields')
     @formField('block_editor', [
-        'blocks' => ['paragraph', 'article-paragraph-h3', 'file-download-button', 'double-paragraph', 'article-title', 'article-image', 'article-introduction', 'article-hero-image', 'latest-cv-download-button']
+        'blocks' => [
+            'paragraph',
+            'article-paragraph-h3',
+            'file-download-button',
+            'double-paragraph',
+            'article-title',
+            'article-image',
+            'article-introduction',
+            'article-hero-image',
+            'article-hero-image-no-lazy',
+            'latest-cv-download-button',
+        ]
     ])
 @stop
