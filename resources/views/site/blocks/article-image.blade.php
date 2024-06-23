12895cbbf5fc770ffb38b652937476c00e05be56
Armin Salihović <armin.salihovic@live.com>
2024-06-23 02:45:43 +0200

    Fix article image frame

--- resources/views/site/blocks/article-image.blade.php
+++ resources/views/site/blocks/article-image.blade.php
@@ -4,14 +4,16 @@
 @endphp
 
 <x-block-layout>
-    <x-lazy-loading-wrapper class="mb-8 md:mb-16 3xl:mb-20 4xl:mb-32" :lqip="$block->lowQualityImagePlaceholder('image')">
-        <img
-            src="{{ $block->image('image') }}"
-            alt="{{ $block->imageAltText('image') }}"
-            loading="lazy"
-            class="w-full h-full {{ $block->input('frame') ? 'block-image-frame' : '' }}"
-            style="aspect-ratio: {{ "$width/$height" }};"
-        >
-    </x-lazy-loading-wrapper>
+    <div class="mb-8 md:mb-16 3xl:mb-20 4xl:mb-32 {{ $block->input('frame') ? 'block-image-frame' : '' }}">
+        <x-lazy-loading-wrapper :lqip="$block->lowQualityImagePlaceholder('image')">
+            <img
+                src="{{ $block->image('image') }}"
+                alt="{{ $block->imageAltText('image') }}"
+                loading="lazy"
+                class="w-full h-full"
+                style="aspect-ratio: {{ "$width/$height" }};"
+            >
+        </x-lazy-loading-wrapper>
+    </div>
 </x-block-layout>
 
